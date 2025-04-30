2a14296bbbc8fa294034d28d1dd8256c1f524bf2
赵高 <114843013+1uglyman1@users.noreply.github.com>
2025-04-30 13:53:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
 import os
-try:
-  os.system("git clone https://github.com/shmilylty/OneForAll.git")
-except:
-  pass
-os.system("dir")
+
+
+def traverse_directory(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            print(file_path)
+
+
+if __name__ == "__main__":
+    try:
+    os.system("git clone https://github.com/shmilylty/OneForAll.git")
+  except:
+    pass
+  os.system("dir")
+  os.system("dir")
+  current_directory = os.getcwd()
+  traverse_directory(current_directory)
+  
